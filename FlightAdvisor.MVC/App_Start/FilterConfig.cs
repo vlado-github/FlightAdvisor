21f573d5c93d1bcadf8ddbd67da2e56d46467f4c
Vladimir MArinkovic <marinkovicvlado@gmail.com>
2020-09-26 14:00:40 +0200

    Solution setup (init)

---
+++ FlightAdvisor.MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FlightAdvisor.MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
